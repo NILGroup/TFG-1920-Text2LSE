8813803ee9f1974eeeaab955cc5cc7fae7aa1d46
Alex <55435088+AlejandroTorralboFuentes@users.noreply.github.com>
2020-06-26 18:54:39 +0200

    Add files via upload

--- Web/scripts.js
+++ Web/scripts.js
@@ -1,6 +1,32 @@
+// Inicializamos el contenido de la web
+$(document).ready(function () {
+	$('#spinner').hide();
+	$('#videoContainer').hide();
+	$('#JSONContainer').hide();
+	$('#imageContainer').hide();
+
+	if ($("body").height() < $(window).height()){
+		$("footer").css({"position":"relative", "bottom" : "0px"});
+	}
+
+	var _originalSize = $(window).width() + $(window).height()
+    $(window).resize(function() {
+        if ($(window).width() + $(window).height() != _originalSize) {
+            console.log("keyboard active");
+            $("footer").removeClass("fixed");
+        } else {
+            console.log("keyboard closed");
+            $("footer").addClass("fixed");
+        }
+    });
+
+});
+
+var ruta = 'http://127.0.0.1:8080/';
+//var ruta = 'https://holstein.fdi.ucm.es/tfg-text2lse/';
 
 // ------------------------------------------------------------------------------------------------------------------------------------------
-// --------------------------------------------------------------- FETCH --------------------------------------------------------------------
+// 							CONTROL ERRORES
 // ------------------------------------------------------------------------------------------------------------------------------------------
 
 // Control de errores en las llamadas Fetch
@@ -8,17 +34,49 @@ function handleErrors(response){
 
 	if(!response.ok) {
 		console.log('Error handleErrors: ' + response.statusText);
-		alert(response.statusText); 
-		location.reload();
 	}
 	return response;
 }
 
+//---------------------------------------------------------------------------------------------------------------------------------
+// 										SELECTOR DE TRADUCTOR
+//---------------------------------------------------------------------------------------------------------------------------------
+
+function Translate (){
+	var formato = $('#formato option:selected').text();
+
+	// introducido texto
+
+	$('#videoContainer').hide();
+	$('#JSONContainer').hide();
+	$('#imageContainer').hide();
+	$('#spinner').show();
+
+	if (formato == "Vídeo")
+	{
+		//GetVideoSentence();
+		GetSentenceVideo();
+	}
+	else if (formato == "Texto")
+	{
+		GetTextLSEJson();
+	} 
+	else if (formato == "Imagen")
+	{
+		GetImageSentence();
+	}
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 										TEXTO TRADUCIDO A TEXTO LSE
+//---------------------------------------------------------------------------------------------------------------------------------
+
 // Llamada a TextoLSE POST - > Devuelve Json
 function GetTextLSEJson(){
 
-	var text = $('#textToTranslateJSON').val();
-	var url_api = 'http://127.0.0.1:8080/TextoLSE/';
+	var text = $('#textToTranslate').val();
+	var url_api = ruta + 'TextoLSE/';
 	event.preventDefault();
 	
 	var data = new FormData();
@@ -36,7 +94,6 @@ function GetTextLSEJson(){
 			if(response.status != 200)
 			{
 				console.log('Error: ' + response.status);
-				// Mostrar error
 			}
 			else{
 				return response.json();
@@ -44,25 +101,31 @@ function GetTextLSEJson(){
 			
 		})
 	.then(function(text){ 
+		$('body').removeClass('disableGrey');
+		$('#spinner').hide();
+		$('#JSONContainer').show();
 		$('#JSONText').html(text.texto.toUpperCase())
-		//alert(text.texto);
-		// Hacer lo que se tenga que hacer con el texto.
 	})
 	.catch(
 		function(error){
-			// Mostrar error
+			$('body').removeClass('disableGrey');
+			$('#spinner').hide();
 			console.log(error);
 		});
 
 }
 
-// Llamada a TextoLSEVideos POST- > Devuelve Json
-function GetTextLSEVideosJson(){
+//---------------------------------------------------------------------------------------------------------------------------------
+// 										IMÁGENES 
+//---------------------------------------------------------------------------------------------------------------------------------
 
+// Llamada a textoImagen POST - > Devuelve Json con el texto traducido según las imágenes existentes
+function GetImageSentence(){
+	$('#imageContainer').html('');
 	var text = $('#textToTranslate').val();
-	var url_api = 'http://127.0.0.1:8080/TextoLSEVideos/';
+	var url_api = ruta + 'imagenes/';
 	event.preventDefault();
-
+	
 	var data = new FormData();
 	data.append("Texto", text)
 
@@ -74,62 +137,195 @@ function GetTextLSEVideosJson(){
 	.then(handleErrors)
 	.then( 
 		function(response){
-			
+
 			if(response.status != 200)
 			{
-				console.log('Error: ' + response.status);
-				// Mostrar error
+				return response.text();
 			}
 			else{
-				return response.json();
+				return response.text();
 			}
 			
 		})
-	.then(function(text){ 
-		$('#JSONContainer').html(text.texto)
-		//alert(text.texto);
-		// Hacer lo que se tenga que hacer con el texto.
+	.then(function(body){
+		debugger;
+		var bodyJson = JSON.parse(body);
+
+		if (bodyJson.hasOwnProperty('rutas'))
+		{
+			getImagenes(bodyJson.rutas);
+			$('#imageContainer').show();
+		}
+		else{
+			$('#errorModal').modal('show');
+			$('#textoError').text("Lo sentimos, ha ocurrido un error. Vuelva a intentarlo en unos minutos.");
+			$('body').removeClass('disableGrey');
+			$('#spinner').hide();
+		}
+			
 	})
 	.catch(
 		function(error){
-			// Mostrar error
-			console.log(error);
+			$('#errorModal').modal('show');
+			$('#textoError').text("Lo sentimos, ha ocurrido un error. Vuelva a intentarlo en unos minutos.");
+			$('body').removeClass('disableGrey');
+			$('#spinner').hide();
 		});
 
+	$('body').removeClass('disableGrey');
+	$('#spinner').hide();
 }
 
-// Llamada a /video/<palabra> GET -> Devuelve video
-function GetVideoWord(){
+async function getImagenes(rutas)
+{	
+	for (url_img of rutas){
+		var palabra = url_img.substring(url_img.lastIndexOf('/') + 1);
+		$('#imageContainer').append('<figure class="wide"><img class="imageLSE" src="' + url_img + '"><figcaption><strong>' + palabra.toUpperCase() + '</strong></figcaption></figure>');
+	}
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 								VIDEOS UNO DETRÁS DE OTRO EN CLIENTE
+//---------------------------------------------------------------------------------------------------------------------------------
+
+// Llamada a textoVideo POST -> Devuelve Json con el texto traducido según los videos existentes
+function GetSentenceVideo(){
 
 	var text = $('#textToTranslate').val();
-	var url_api = 'http://127.0.0.1:8080/video/' + text;
+	var url_api = ruta + 'TextoLSEVideos/';
 	event.preventDefault();
+	
+	var data = new FormData();
+	data.append("Texto", text)
 
 	fetch(url_api, {
-	  mode:'cors'
+	  method: "POST",
+	  mode:'cors',
+	  body: data
 	})
 	.then(handleErrors)
 	.then( 
 		function(response){
-			var url = window.URL || window.webkitURL; 
-			response.blob().then(function(video) {
-				var objectUrl = url.createObjectURL(video);
-				$('#videoContainer').html('<video id="video" width="300" height="240" controls><source id="source" src="' + objectUrl + '" type=video/mp4></video>');	
-			})
+
+			if(response.status != 200)
+			{
+				return response.text();
+			}
+			else{
+				return response.text();
+			}
+			
+		})
+	.then(function(body){
+
+		var bodyJson = JSON.parse(body);
+
+		if (bodyJson.hasOwnProperty('frase'))
+		{
+			getVideos(bodyJson.frase);
+			
+		}
+		else{
+			$('#errorModal').modal('show');
+			$('#textoError').text("Lo sentimos, ha ocurrido un error. Vuelva a intentarlo en unos minutos.");
+			$('body').removeClass('disableGrey');
+			$('#spinner').hide();
+		}
+			
 	})
 	.catch(
 		function(error){
-			// Mostrar error
-
+			$('#errorModal').modal('show');
+			$('#textoError').text("Lo sentimos, ha ocurrido un error. Vuelva a intentarlo en unos minutos.");
+			$('body').removeClass('disableGrey');
+			$('#spinner').hide();
 		});
+
+	$('body').removeClass('disableGrey');
+	$('#spinner').hide();
 }
+
+// Funciones globales para video
+	var vidSources = [];
+	var nextVideo = "", fraseVideo = "";
+	var nextActiveVideo = 0;
+	var videoObjects = [
+			$('#videoLSE1'),
+			$('#videoLSE2')
+		];
+
+// Función que muestra los videos uno detrás de otro
+async function getVideos(frase)
+{	
+	videoObjects = [
+			$('#videoLSE1'),
+			$('#videoLSE2')
+		];
+	fraseVideo = frase;
+	
+	var url_api = ruta + 'video/';
+	nextActiveVideo = 0;
+	vidSources = [];
+
+	for (palabra of frase){
+		vidSources.push(url_api + palabra);
+	}
+
+	videoObjects[0][0].inx = 0; 
+	videoObjects[1][0].inx = 1;
+
+	initVideoElement(videoObjects[0][0]);
+	initVideoElement(videoObjects[1][0]);
+
+	videoObjects[0][0].autoplay = true;
+	videoObjects[0].attr("src", vidSources[nextActiveVideo]);
+	$('#txtVideo').text(fraseVideo[nextActiveVideo].toUpperCase());
+	$('#videoContainer').show();
+}
+
+function initVideoElement(video)
+{
+    video.playsinline = true;
+    video.muted = false;
+    video.preload = 'auto'; 
+
+    video.onplaying= function()
+    {
+        nextActiveVideo = ++nextActiveVideo % vidSources.length;
+
+        if(this.inx == 0){
+            nextVideo = videoObjects[1][0];
+        }
+        else
+            nextVideo = videoObjects[0][0];
+
+        nextVideo.src = vidSources[nextActiveVideo];
+        nextVideo.load();
+        nextVideo.pause();
+    };
+
+    video.onended = function()
+    {
+        this.style.display = 'none';
+        nextVideo.style.display = 'inline';
+        $('#txtVideo').text(fraseVideo[nextActiveVideo].toUpperCase());
+        if (nextActiveVideo != 0)
+        {
+        	nextVideo.play();
+        }
+        
+    };
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 								VIDEOS JUNTOS EN SERVIDOR
+//---------------------------------------------------------------------------------------------------------------------------------
 
 // Llamada a /video POST -> Devuelve video
 function GetVideoSentence(){
 	$('#videoContainer video').remove();
-	$("#videoContainer").html('<div class="loader center"></div>');
-	var text = $('#textToTranslateVideo').val();
-	var url_api = 'http://127.0.0.1:8080/video/';
+	var text = $('#textToTranslate').val();
+	var url_api = ruta + 'video/';
 	event.preventDefault();
 
 	var data = new FormData();
@@ -147,26 +343,31 @@ function GetVideoSentence(){
 			if(response.status != 200)
 			{
 				console.log('Error: ' + response.status);
-				// Mostrar error
+				return response.text();
+				
 			}
 			else{
 				var url = window.URL || window.webkitURL; 
 				response.blob().then(function(video) {
-				var objectUrl = url.createObjectURL(video);
-				$('#videoContainer').html('<video id="video" class="embeb-responsive-item" autoplay controls><source id="source" src="' + objectUrl + '" type=video/mp4></video>');	
-			})
+					var objectUrl = url.createObjectURL(video);
+					$('body').removeClass('disableGrey');
+					$('#spinner').hide();
+					$('#videoContainer').show();
+					$('#videoContainer').html('<video id="video" class="embeb-responsive-item" autoplay controls><source id="source" src="' + objectUrl + '" type=video/mp4></video>');	
+				})
 			}
 			
 		})
+	.then(function(body){
+			// $('#textoError').text(JSON.parse(body).message);
+			// $('body').removeClass('disableGrey');
+			// $('#spinner').hide();
+			// $('#errorModal').modal('show');
+		}
+	)
 	.catch(
 		function(error){
-			// Mostrar error
 			console.log(error);
 		});
 
 }
-
-
-// ---------------------------------- PENDIENTE ------------------------------------
-// 1. Control de errores en las llamadas -> mostrarlos correctamente.
-// 2. Poner spinner para que el usuario sepa que se está cargando.
